c3ee828253af910df2ee80c34f769371e1dd49a3
KevenRafael1 <ckeven79@gmail.com>
2025-05-20 20:51:46 -0300

    k

---
+++ StockAndSales/src/main/java/com/StockAndSales/StockAndSales/StockAndSalesApplication.java
@@ -0,0 +1,13 @@
+package com.StockAndSales.StockAndSales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAndSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAndSalesApplication.class, args);
+	}
+
+}
---
+++ StockAndSales/src/test/java/com/StockAndSales/StockAndSales/StockAndSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StockAndSales.StockAndSales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAndSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
